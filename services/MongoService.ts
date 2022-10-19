fe7b09c603d01f4e6a1145a994551909e3b5f29a
Jakob Pesch <jakob.pesch@rwth-aachen.de>
2022-10-19 12:20:16 +0300

    add connection string for mongo db

--- services/MongoService.ts
+++ services/MongoService.ts
@@ -1,7 +1,8 @@
 // connection.js
 import mongoose from "mongoose"
 
-const connection = "mongodb://localhost:27017/mongo-schnoz"
+const connection =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/mongo-schnoz"
 
 const connectDb = () => {
   return mongoose.connect(connection)
