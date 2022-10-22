03f2289790e2677ba237121ff2c58d13954d9c3f
Jakob Pesch <jakob.pesch@rwth-aachen.de>
2022-10-22 20:37:52 +0300

    add finished state

--- pages/api/match/[id]/moves.ts
+++ pages/api/match/[id]/moves.ts
@@ -4,6 +4,8 @@ import connectDb from "../../../../services/MongoService"
 import { IMap } from "../../../../models/Map.model"
 import Match from "../../../../models/Match.model"
 
+export type MatchStatus = "created" | "started" | "finished"
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<IMap>
@@ -38,17 +40,23 @@ export default async function handler(
         res.status(500).end("Tile is blocked by another unit.")
         break
       }
+
       match.map.tiles[tileIndex] = {
         ...match.map.tiles[tileIndex],
         unit: { playerId: userId },
       }
+
       match.activePlayer = match.players.find(
         (playerId: string[]) => playerId !== match.activePlayer
       )
+
       match.turn = (match.turn ?? 0) + 1
-      if (match.turn > match.maxTurns) {
+
+      const isGameFinished = match.turn > match.maxTurns
+      if (isGameFinished) {
         match.status = "finished"
       }
+
       await match.save()
       res.status(201).json(match)
       break
